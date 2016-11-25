9ce42d7c8ddfd217142e552cde2cef2f7cccd8fe
wang987 <1449076200@qq.com>
2016-11-25 13:47:52 +0800

    实体类

--- scms/src/com/entity/Product.java
+++ scms/src/com/entity/Product.java
@@ -1,30 +1,48 @@
 package com.entity;
 
 public class Product {
-
-	public Object getPrice() {
-		// TODO Auto-generated method stub
-		return null;
+	private Integer productId;
+	private String name;
+	private Integer count;
+	private Float bid;
+	private Float price;
+	private Float profit;
+	public Integer getProductId() {
+		return productId;
 	}
-
-	public Object getProductId() {
-		// TODO Auto-generated method stub
-		return null;
+	public void setProductId(Integer productId) {
+		this.productId = productId;
 	}
-
-	public Object getName() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getName() {
+		return name;
 	}
-
-	public void setName(Object name) {
-		// TODO Auto-generated method stub
-		
+	public void setName(String name) {
+		this.name = name;
 	}
-
-	public void setPrice(Object price) {
-		// TODO Auto-generated method stub
-		
+	public Integer getCount() {
+		return count;
 	}
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+	public Float getBid() {
+		return bid;
+	}
+	public void setBid(Float bid) {
+		this.bid = bid;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	public Float getProfit() {
+		return profit;
+	}
+	public void setProfit(Float profit) {
+		this.profit = profit;
+	}
+	
 
 }
